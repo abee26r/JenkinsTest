c80cb630fe696bdce5d0d788468458c1e6d23fc4
abee26r <ab.almighty@gmail.com>
2015-02-28 20:09:56 -0500

    Junit

---
+++ JenkinTest/src/com/abee/ProcessSomethingTest.java
@@ -0,0 +1,14 @@
+package com.abee;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProcessSomethingTest {
+
+	@Test
+	public void testProcess() {
+		fail("Not yet implemented");
+	}
+
+}
